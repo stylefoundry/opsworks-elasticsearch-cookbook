df4219b469a431693e7bdda1a74b7d852cbc3ebe
Justin Boynton <justin.boynton@stylefoundry.co.uk>
2016-02-17 14:15:56 +0000

    Update default elastic search

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,3 +1,2 @@
-normal[:elasticsearch][:version] = '1.7.0'
-normal[:elasticsearch][:plugins]['elasticsearch/elasticsearch-cloud-aws'][:version] = '2.7.0' # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
+normal[:elasticsearch][:version] = '2.1.0'
 normal[:elasticsearch][:cluster][:name] = 'sweet_cluster'  # Change to whatever you want.
